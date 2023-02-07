e7aaa8f161f127a5c40892fe06c717436caa5c77
Isis <1036029@hr.nl>
2023-02-07 01:29:33 +0100

    WIP branch

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,27 @@ import { useState, useEffect } from 'react';
 import './scss/styles.scss';
 import { Layout } from './components/Layout';
 import { CardDetail } from './components/Card/CardDetail';
-import { Error } from './components/Error';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Error } from './Pages/Error';
+import { Home } from './Pages/Home';
+import { BrowserRouter, useParams, Routes, Route } from 'react-router-dom';
 import { EditCard } from './components/Card/EditCard';
 import { CreateCard } from './components/Card/CreateCard';
 import { BoardLayout } from './components/BoardLayout';
 import io from 'socket.io-client';
 
+
 const socket = io.connect('http://localhost:3000');
 
 export const ThemaContext = React.createContext();
 
+
 export function App() {
+    const params = useParams()
     const [lists, setLists] = useState([]);
     const [pagination, setPagination] = useState([]);
-    const [url, setUrl] = useState('https://prg06.iettech.nl/lists');
+    const id = `https://prg06.iettech.nl/boards/${params.id}`;
+
+    const [url, setUrl] = useState("");
 
     useEffect(() => {
         socket.on('sendUpdate', () => {
@@ -32,6 +38,8 @@ export function App() {
     }, []);
 
     const loadBoard = () => {
+        if (!url) return
+
         fetch(url, {
             method: 'get',
             headers: {
@@ -46,19 +54,24 @@ export function App() {
     useEffect(loadBoard, [url]);
 
     const handleResponse = (data) => {
+        return console.log(data);
         setLists(data.items);
         setPagination(data.pagination);
     }
+
 
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Layout />}>
-                    <Route path="/cards" element={<BoardLayout lists={lists} pagination={pagination} socket={socket} cardRefreshHandler={() => loadBoard()} changePageHandler={setUrl} />}>
-                        <Route path=":id/create" element={<CreateCard lists={lists} socket={socket} cardRefreshHandler={() => loadBoard()} />} />
+                    <Route path="/" element={<Home />} />
+                    // Board
+                    <Route path="board/:id" element={<BoardLayout lists={lists} pagination={pagination} socket={socket} cardRefreshHandler={() => loadBoard()} changePageHandler={setUrl} />}>
+                        {/* <Route path=":id/create" element={<CreateCard lists={lists} socket={socket} cardRefreshHandler={() => loadBoard()} />} />
                         <Route path=":id/edit" element={<EditCard lists={lists} socket={socket} cardRefreshHandler={() => loadBoard()} />} />
-                        <Route path=":id" element={<CardDetail lists={lists} cardRefreshHandler={() => loadBoard()} />} />
+                        <Route path="c/:id" element={<CardDetail lists={lists} cardRefreshHandler={() => loadBoard()} />} /> */}
                     </Route>
+
                     <Route path="*" element={<Error />} />
                 </Route>
             </Routes>
